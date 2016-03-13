703e074ba10116ead5031d703df40c10f90eab79
Aaron Quek <QUEK0158@e.ntu.edu.sg>
2016-03-12 23:06:33 -0500

    First Commit
    Added VR Scene

---
+++ VRAuburn/Assets/Controllers/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	Vector3[] pos;
+
+	GameObject cam;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Game Start");
+//		camPositions[0] = (13,15,-1000);
+
+		cam = GameObject.Find("Cardboard Main");
+
+		pos = new[] {new Vector3( 13, 11, -950 ),
+			new Vector3 (13, 11, -450 )};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1"))
+		{
+			Debug.Log("Fire Clicked!");
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit rayHit;
+
+			if(Physics.Raycast(ray, out rayHit)){
+				Debug.Log(rayHit.transform.name);
+				if(rayHit.transform.name == "Enter Tour"){
+					Debug.Log("Enter Tour hit!");
+					cam.transform.position = Vector3.Lerp(this.transform.position, pos[1],(float)0.2);
+				}
+
+			}
+		}
+
+
+	}
+}
---
+++ VRAuburn/Assets/Controllers/MLK1Controller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MLK1Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetButtonDown("Fire1")){
+			transform.Translate(new Vector3(0f,0f,-10f));
+		}
+	}
+}
